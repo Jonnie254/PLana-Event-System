990de87f11d220e6aea3aa9c2115f40c1afaf707
jonnie254 <johnkamanu254@proton.me>
2024-07-16 07:22:59 +0300

    added code

--- frontend/ticketing-system/tailwind.config.js
+++ frontend/ticketing-system/tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: { poppins: ["Poppins", "sans-serif"] },
+      animation: {
+        "fade-in-up": "fade-in-up 0.5s ease-out forwards",
+      },
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
